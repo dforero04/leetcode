6b2914daf7a13a89dd7fc41be433ae89e24c6afd
dforero04 <dxforero@ualr.edu>
2022-06-21 19:34:30 -0500

    Starting to track my Leetcode problems

---
+++ 100.same-tree.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=100 lang=javascript
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+//This is more of a BFS because you are checking the root first
+var isSameTree = function (p, q) {
+  // Check to see if TreeNodes are null
+  if (!p && !q) return true;
+  // Check to see if either or is null
+  // Example: This checks the case if one tree has a leaf and the other doesn't
+  if (!p || !q) return false;
+  // Check the actual values of the node
+  if (p.val !== q.val) return false;
+  // Recursively check left and right sides of tree
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
---
+++ 20.valid-parentheses.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const map = new Map({ ")": 0, "}": 0, "]": 0 });
+  console.log(map);
+};
+// @lc code=end
